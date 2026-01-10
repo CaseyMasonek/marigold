683dc4a3ec55bbf69cccf82643e4670ced112204
Casey Masonek <cmmasonek@gmail.com>
2026-01-10 10:38:08 -0800

    Update CLI

--- marigold/cli.py
+++ marigold/cli.py
@@ -3,17 +3,42 @@ def main():
     from marigold.compiler import Compiler
     from marigold.lcode import lambdacode
     import sys
+    import os
+
+    if (sys.argv[1]) == "new":
+        if (len(sys.argv) < 3):
+            print("Missing required argument: project name")
+            return
+
+        project_dir = sys.argv[2]
+
+        os.makedirs(project_dir, exist_ok=True)
+
+        with open(project_dir + "/main.mg","w") as f:
+            f.write('put "Hello, World!";')
+
+        with open(project_dir + "/pkginfo.json","w") as f:
+            f.write(f"""{{
+    "packageName": "{project_dir}",
+    "requirements": []
+}}""")
+        
+        return
+
 
     with open(sys.argv[1]) as f:
         tree = parser.parse(f.read())
-
-        print(tree.pretty())
+        
+        if '-d' in sys.argv:
+            print(tree.pretty())
 
         c = Compiler('./' + sys.argv[1]).transform(tree)
+        
+        if '-d' in sys.argv:
+            print("\nCode to run:",c)
 
-        print("\nCode to run:",c)
-
-        print('-'*10,'OUTPUT','-'*10)
+        if '-d' in sys.argv:
+            print('-'*10,'OUTPUT','-'*10)
 
         namespace = {}
         exec(lambdacode, namespace) 
--- src/main.mg
+++ src/main.mg
@@ -1,5 +1,19 @@
-importall Queue;
+def say(n) {
+    if (n % 15 == 0) {
+        put "fizzbuzz";
+    } elif (n % 3 == 0) {
+        put "fizz";
+    } elif (n % 5 == 0) {
+        put "buzz";
+    } else {
+        putint n;
+    }
+}
 
-Queue 5
-| Enqueue "hello"
-| Describe put;
+def fizzbuzz(n) {
+    (RANGE n)
+    | MAP SUCC
+    | MAPL say;
+}
+
+fizzbuzz 100;
